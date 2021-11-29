7981e0dcdf5711ef5a8f467ad388746767822ff8
KimJinWooDa <76438011+KimJinWooDa@users.noreply.github.com>
2021-11-29 17:39:08 +0900

    Create 1152.cs

---
+++ 1152.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BaekJoon
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string str = Console.ReadLine().Trim();
+            string[] arr = str.Split('\x020');
+            int count = 0;
+         
+            for(int i = 0; i < arr.Length; i++)  count++;
+            if (str.Length == 0) count = 0;
+            Console.WriteLine(count);
+        }
+    }
+}
